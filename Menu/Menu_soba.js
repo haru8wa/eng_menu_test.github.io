0cf65e2cd3ea1a39a59cb9fe34d0e96a53b417ed
haru8wa <titatoru3618@gmail.com>
2025-05-04 15:32:59 +0900

    Add files via upload

---
+++ Menu/Menu_soba.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    var param = {
+        url : 'Menu_soba_recommended.json',
+        dataType : 'json'
+    }
+    $.ajax(param).done(function(ary){
+        var html = '';
+        for(var data of ary){
+            html += '<div class="ItemImageSet"><div class="ReccItem"><div class="ItemName">' + data.name + '</div><br>';
+            if(data.ingredients)
+                html +=  data.ingredients + '</div><img src="' + data.image + '" class="ItemImage"></div><br>';
+            else if(data.variations)
+                html +=  data.variations + '</div><img src="' + data.image + '" class="ItemImage"></div><br>';
+            else 
+                html +=  data.items + '</div><img src="' + data.image + '" class="ItemImage"></div><br>';
+        }
+        $('#ReccMenu').html(html);
+    });
+});
